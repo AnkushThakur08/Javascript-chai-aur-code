732318cc2c1f985908146671b6525a92dfcfb43a
Ankush Thakur <66546775+AnkushThakur08@users.noreply.github.com>
2024-01-31 13:28:49 +0530

    Data types

---
+++ basics/02dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+
+let name = "userName"
+let age = 25
+let isLoggedIn = false
+let state;
+
+// number 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
