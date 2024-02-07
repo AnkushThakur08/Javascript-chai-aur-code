6980a2a26bfdf260a92dbc461353df1422c1864c
Ankush Thakur <66546775+AnkushThakur08@users.noreply.github.com>
2024-02-07 11:04:29 +0530

    Conversion

---
+++ basics/03conversion.js
@@ -0,0 +1,19 @@
+let value = ""
+
+
+// console.log(value);
+// console.log(typeof value);
+
+// Conversion
+
+let valueInNumber = Number(value);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+/* 
+    "33" - 33
+    "33abc" - nan
+     "" - 0 
+     undefined - nan
+     null - 0
+*/
