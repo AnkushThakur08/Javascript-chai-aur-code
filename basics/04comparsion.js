638a49a82e3081f779d32b37df292dfe3b82c3da
Ankush Thakur <66546775+AnkushThakur08@users.noreply.github.com>
2024-02-07 11:28:06 +0530

    comparsion and null

---
+++ basics/04comparsion.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); /* False */
+console.log(null == 0); /* false */
+console.log(null >= 0); /* True, as when we are checking the value, null is considered as 0  */
+// Interview Question
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
