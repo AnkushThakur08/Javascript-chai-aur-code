d2d247e7db52646ec5fefaa17c9972a01f16c7d7
Ankush Thakur <66546775+AnkushThakur08@users.noreply.github.com>
2024-02-07 12:44:06 +0530

    data types added

---
+++ basics/05datatypes.js
@@ -0,0 +1,46 @@
+let value = Symbol("123");
+const value2 = Symbol("123");
+
+console.log(value);
+console.log(value2);
+console.log(value === value2);
+
+/* Symbol is unique, IT IS used for uniquess */
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
