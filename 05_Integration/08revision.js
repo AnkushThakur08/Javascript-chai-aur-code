4d430195867993341b9fc6b8845f3c6cb8a2be2a
Ankush Thakur <66546775+AnkushThakur08@users.noreply.github.com>
2024-04-24 07:00:19 +0000

    code updated

---
+++ 05_Integration/08revision.js
@@ -0,0 +1,18 @@
+// for(i=0; i<=5; i++){
+//     console.log(i)
+// }
+
+/* Print Table */
+// for(i=1; i<=10; i++){
+//     for(j=1; j<=10; j++)
+//     console.log(`${i} * ${j}  = ${i * j} `)
+// }
+
+
+/* Array Item */
+
+const arr = ["Amazon", "Microsoft", "Apple", "Zoho"];
+
+for(i=0; i<arr.length; i++){
+    console.log(arr[i])
+}
